c03901b9dc59651f09773712e55051ae02adb93c
Robert Tung <rrt23@tangra>
2017-09-26 00:18:56 -0400

    basic parse

--- xmltotxtparagraph.py
+++ xmltotxtparagraph.py
@@ -1,4 +1,5 @@
 import sys
+import xml.etree.ElementTree as ET
 
 def main():
 	if len(sys.argv) < 2:
@@ -6,17 +7,17 @@ def main():
 		return 1
 
 	xmlFileName = sys.argv[1]
-	if len(xmlFileName) < 4 or xmlFileName[-4:] != ".xml":
+	if len(xmlFileName) < 4 or (xmlFileName[-4:] != ".xml" and xmlFileName[-5:] != ".nxml"):
 		print("Invalid file name.")
 		print("Format: python xmltotxtparagraph.py <xmlfilename>.xml")
 		return 1
 
-	try:
-		xmlFile = open(xmlFileName,"r")
-	except IOError:
-		print("Invalid file name.")
-		print("Format: python xmltotxtparagraph.py <xmlfilename>.xml")
-		return 1
+	# try:
+	# 	xmlFile = open(xmlFileName,"r")
+	# except IOError:
+	# 	print("Invalid file name.")
+	# 	print("Format: python xmltotxtparagraph.py <xmlfilename>.xml")
+	# 	return 1
 
 	try:
 		txtFile = open(sys.argv[1] + ".txt","w")
@@ -24,12 +25,15 @@ def main():
 		print("Error opening output file.")
 		return 1
 
-	
+	tree = ET.parse(xmlFileName)
+	root = tree.getroot()
+	for para in root.findall('p'):
+		txt.File.write(para.text)
 
-	xmlFile.close()
+	# xmlFile.close()
 	txtFile.close()
 
-	print("hello")
+	print("Done.")
 	return 0
 
 if __name__ == "__main__":
