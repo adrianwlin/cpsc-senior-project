4823c4f117463d90ac9b37855cbed760deb0f883
adrianwlin <linthedrummer@gmail.com>
2017-11-27 18:03:15 -0500

    Looks up gene and disease associations in the disgenet db

--- scripts/cytoscape_lookup.py
+++ scripts/cytoscape_lookup.py
@@ -1,5 +1,6 @@
 import sqlite3
 import sys
+import pickle
 from sqlite3 import Error
 
 
@@ -17,32 +18,75 @@ def create_connection(db_file):
     return None
 
 
-def query(connection):
+def label_data(pickle_file, outfile, connection):
     """
     Query all rows in the tasks table
     :param conn: the Connection object
     :return:
     """
-    cur = connection.cursor()
-    cur.execute("SELECT * FROM geneAttributes LIMIT 10")
+    with open(pickle_file, "r") as f:
+        labeled_list = pickle.load(f)
+        no_good = 0
+        good = 0
+        positive = 0
+        negative = 0
+        for entry in labeled_list:
+            if len(entry["genes"]) == 0 or len(entry["diseases"]) == 0:
+                no_good += 1
+            else:
+                good += len(entry["genes"]) * len(entry["diseases"])
+                for gene in entry["genes"]:
+                    uniprot = gene["uniprot"]
+                    if not uniprot:
+                        continue
+                    cur = connection.cursor()
+                    cur.execute(
+                        "SELECT geneNID FROM geneAttributes WHERE uniprotId = \"{}\"".format(uniprot))
+                    rows = cur.fetchall()
+                    if len(rows) != 1:
+                        continue
+                    geneNID = rows[0][0]
 
-    rows = cur.fetchall()
+                    for disease in entry["diseases"]:
+                        cui = disease["cui"]
+                        cur = connection.cursor()
+                        cur.execute(
+                            "SELECT diseaseNID FROM diseaseAttributes WHERE diseaseId = \"{}\"".format(cui))
+                        rows = cur.fetchall()
+                        if len(rows) != 1:
+                            continue
+                        diseaseNID = rows[0][0]
 
-    for row in rows:
-        print(row)
+                        cur = connection.cursor()
+                        cur.execute(
+                            "SELECT associationType FROM geneDiseaseNetwork WHERE geneNID = \"{}\" AND diseaseNID = \"{}\"".format(geneNID, diseaseNID))
+                        rows = cur.fetchall()
+                        if len(rows) == 1:
+                            positive += 1
+                        else:
+                            negative += 1
+
+        print no_good, good
+        print "positive: {}, negative {}".format(positive, negative)
 
 
 def main():
-    """cytoscape_lookup.py [db_file]
+    """cytoscape_lookup.py [db_file] [pickle_file] [outfile]
     """
-    if len(sys.argv) != 2:
-        print "cytoscape_lookup.py [db_file]"
+    # Example code for reading pickle file
+    # f = open(pickleDumpFile,'r')
+    # testLoad = pickle.load(f)
+    # print(testLoad == entityList)
+    if len(sys.argv) != 4:
+        print "cytoscape_lookup.py [db_file] [pickle_file] [outfile]"
         return 1
     else:
         db_file = sys.argv[1]
+        pickle_file = sys.argv[2]
+        outfile = sys.argv[3]
         connection = create_connection(db_file)
         with connection:
-            query(connection)
+            label_data(pickle_file, outfile, connection)
     return 0
 
 
